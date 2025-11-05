4a6ed9e9191217626e20aa1562a7531e8160107e
Abolfazl-JD <abolfazljdst@gmail.com>
2025-11-05 17:38:09 +0330

    feat: add auth controller logics

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,30 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, HttpStatus, Post } from "@nestjs/common";
+import { AuthService } from "./auth.service";
+import { Public } from "src/common";
+import { LoginDto, RegisterDto } from "./dto";
 
-@Controller('auth')
-export class AuthController {}
+@Public()
+@Controller("auth")
+export class AuthController {
+    constructor(private readonly authService: AuthService) {}
+
+    @Post("register")
+    async register(@Body() dto: RegisterDto) {
+        const { access_token } = await this.authService.register(dto);
+        return {
+            statusCode: HttpStatus.CREATED,
+            access_token,
+        };
+    }
+
+    @Post("login")
+    async login(@Body() dto: LoginDto) {
+        const user = await this.authService.validateUser(dto);
+
+        const { access_token } = await this.authService.login(user);
+        return {
+            statusCode: HttpStatus.CREATED,
+            access_token,
+        };
+    }
+}
